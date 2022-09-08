f2233934ab09f2ab68737b548a1b0cb8e5736066
Artsiom Trushko <artemchiik@gmail.com>
2022-09-08 21:51:07 +0300

    solved

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-
+        for (int i = 1; i < numberToGoUntil; i++) {
+            if (numberToGoUntil < toBreakWith) {
+                System.out.println("iterating till the end");
+                do {
+                    System.out.println(i);
+                    i++;
+                }
+                while (i <= numberToGoUntil);
+                break;
+            }
+            if (i > toBreakWith) {
+                break;
+            }
+            System.out.println(i);
+        }
     }
 }
